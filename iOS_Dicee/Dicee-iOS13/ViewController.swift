d5330fa42591e39dfb24e8ab00e3fc9a8613fb7e
Snehal Utage <snehal19su@gmail.com>
2022-02-06 11:28:54 -0600

    Added Dice roll project

---
+++ iOS_Dicee/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //IBOutlet allows to reference to a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+        //Create the dice images array, used ImageLiteral to get the images in array
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        //Use random generator to get the random image from the diceArray
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray.randomElement()
+    }
+}
+
